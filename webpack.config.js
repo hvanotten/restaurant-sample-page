4391763b482c1d69e7a9f25bb9e9de6cb916b87c
Hillary <hillaryvanotten@gmail.com>
2022-04-24 00:10:09 -0700

    Updating styling

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
 
 module.exports = {
-    mode: "development",
-    entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
 
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/restaurant-sample-page/",
+  },
 };
